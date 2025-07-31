7e7aeab7a5b7cb902e5e4c4eee385fb0fd2fe700
shadrack <phoenixnova427@gmail.com>
2025-07-31 14:35:49 +0300

    my first plp py program

---
+++ calc.py
@@ -0,0 +1,22 @@
+num1 = input("Enter your first number:")
+op = input("Enter the operation:")
+num2 = input("Enter your second number:")
+
+num1 = int(num1)
+num2 = int(num2)
+if op == "+":
+    print(f"{num1} {op} {num2} = {num1+num2}")
+elif op == "-":
+    print(f"{num1} {op} {num2} = {num1-num2}")
+elif op == "*":
+    print(f"{num1} {op} {num2} = {num1*num2}")
+elif op == "/":
+    print(f"{num1} {op} {num2} = {num1/num2}")
+elif op == "//":
+    print(f"{num1} {op} {num2} = {num1//num2}")
+elif op == "**":
+    print(f"{num1} {op} {num2} = {num1**num2}")
+elif op == "%":
+    print(f"{num1} {op} {num2} = {num1%num2}")
+else:
+    print("invalid operation")
