53cf560401b4ab6a29aba03a64ccac679377d656
shadrack <phoenixnova427@gmail.com>
2025-08-22 00:55:03 +0300

    PLP wk2 assignment

---
+++ list.py
@@ -0,0 +1,18 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list[1] = 15
+
+list2 = [50, 60, 70]
+my_list.extend(list2)
+
+my_list.pop(-1)
+
+my_list.sort()
+
+index = my_list.index(30)
+print(index)
